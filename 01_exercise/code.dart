bea750bd2d606c57afb4d24e4feb5eabde8b20df
mensur056 <mansur.sarxanov@gmail.com>
2023-03-10 19:07:39 +0400

    write exercise

---
+++ 01_exercise/code.dart
@@ -0,0 +1,31 @@
+
+
+//EXERCISE 
+// Bildiyin butun tiplere aid deyisenler yaz ve onlara ozun bir qiymet ver
+
+
+
+
+// EXERCISE 1
+// Iki dene list duzelt. Ilk list icinde reqemler olan ikinci list ise icinde adlar olan list olsunlar ve listlere bezi deyerler elave et listin mehtodlari ile
+
+
+
+// EXERCISE 2
+// bir istifadeci yarat adi Mansur olsun ve mansurun birde ayliq qazanci ve ayliq xerci olsun qazanci 600 ,xerci ise 450 dir
+//Mansurun ayliq ozune qalan pulu hesablayacaq bir kod yaz
+
+
+
+// EXERCISE 3
+// Daxilinde String, int .double ,bool olan 4 dene ferqli list duzelt ve bu listler ancaq daxilinde ozlerine mexsus tipler olsun
+// list adlarini istediyini vere bilersen
+
+
+// EXERCISE 4
+// senin bir listin olsun bu listin adin name olsun ve listin icersisinde "Mansur","Emil","Sahin","Tahir","Sadiq" olsun 
+// bu listden Tahiri silmeni ve Tahirin  yerine   Elekberi daxil et ve en sonda listin icinde ne qeder ad oldugunu ekranda goster
+
+
+// EXERCISE 5
+// 
